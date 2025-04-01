ad4ad46c6e38ff6c324c0e1881e98cb8bbb8160e
Mihajlo <nmihajlon@gmail.com>
2025-04-02 01:10:36 +0200

    init push

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return <>
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign in</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  </>;
+};
+
+export default App;
