2537284ff3116c28e5fe136e45ff8f6eee9eb06c
Mihajlo <nmihajlon@gmail.com>
2025-04-03 12:30:10 +0200

    be: songs and stat controllers

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getStats } from "../controller/stat.controller.js";
+import {protectRoute, requireAdmin} from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send("Hello world");
-})
+router.get("/", protectRoute, requireAdmin, getStats);
 
 export default router;
